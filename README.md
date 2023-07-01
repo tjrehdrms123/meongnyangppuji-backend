c1e096373a7617ce114ca76f542c098cc25b033e
tjrehdrms123 <wwe5455@naver.com>
2023-07-01 21:50:08 +0900

    inital

---
+++ README.md
@@ -0,0 +1,24 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Nest-TypeOrm-Board
+
+## NEV Sample
+
+```
+# app
+NODE_ENV=
+PORT=
+ADMIN_USER=
+ADMIN_PASSWORD=
+# JWT
+SECRET_KEY=
+# DB
+DB_USERNAME=
+DB_PASSWORD=
+DB_HOST=
+DB_PORT=
+DB_NAME=
+DB_TYPE=
+```
---
+++ src/app.module.ts
@@ -0,0 +1,51 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
+import * as Joi from 'joi'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
+import { AppController } from './app.controller'
+
+const typeOrmModuleOptions = {
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<Object> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: configService.get('DB_TYPE'),
+    host: configService.get('DB_HOST'), // process.env.DB_HOST
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    entities: [],
+    synchronize: true, //! set 'false' in production
+    autoLoadEntities: true,
+    logging: true,
+    keepConnectionAlive: true,
+  }),
+  inject: [ConfigService],
+}
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(5000),
+        SECRET_KEY: Joi.string().required(),
+        ADMIN_USER: Joi.string().required(),
+        ADMIN_PASSWORD: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}
