21203b7e1fc41473c2a392242daed90c84c38e5d
tjrehdrms123 <wwe5455@naver.com>
2023-07-12 01:11:12 +0900

    delete: 임시 제거

--- src/animal/animal.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AnimalService } from './animal.service';
-import { AnimalController } from './animal.controller';
-
-@Module({
-  controllers: [AnimalController],
-  providers: [AnimalService]
-})
-export class AnimalModule {}
--- src/animal/dto/update-animal.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAnimalDto } from './create-animal.dto';
-
-export class UpdateAnimalDto extends PartialType(CreateAnimalDto) {}
