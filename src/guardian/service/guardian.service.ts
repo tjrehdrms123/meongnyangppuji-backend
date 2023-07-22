afa4e2a467797cd63bff72202b04d4dd58a95a7e
tjrehdrms123 <wwe5455@naver.com>
2023-07-22 16:55:13 +0900

    feat: guardian service

--- src/guardian/service/guardian.service.ts
+++ src/guardian/service/guardian.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { GuardianRepository } from '../infra/guardian.repository';
+import { UpdateGuardianDto } from '../dto/request/update_guardian_dto';
 
 @Injectable()
-export class GuardianService {}
+export class GuardianService {
+  private readonly logger = new Logger(GuardianService.name);
+  constructor(
+    private readonly guardianRepository: GuardianRepository,
+  ) {}
+
+  async createGuardian(guardianData: UpdateGuardianDto) {
+    return await this.guardianRepository.createGuardian(guardianData);
+  }
+
+  async updateGuardian(guardianData: UpdateGuardianDto) {
+    return await this.guardianRepository.updateGuardian(guardianData);
+  }
+}
