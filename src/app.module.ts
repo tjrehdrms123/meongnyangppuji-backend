017f04d1d7ba8928ff8b70ed9a30a07e41ed572a
tjrehdrms123 <wwe5455@naver.com>
2023-07-24 18:18:22 +0900

    feat: moudle fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,6 @@ import { AnimalTypeModule } from './animal_type/animal_type.module';
 import { ThrottlerModule } from '@nestjs/throttler'
 import { GuardianModule } from './guardian/guardian.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 const typeOrmModuleOptions = {
   useFactory: async (
@@ -53,7 +52,6 @@ const typeOrmModuleOptions = {
     AnimalTypeModule,
     GuardianModule,
     UsersModule,
-    AuthModule,
     // ThrottlerModule.forRoot({
     //   ttl: process.env.NODE_ENV === 'production' ? 300 : 60,
     //   limit: 3
