3f2a32ba14853b5a7198efead89f3a7d4716fa5b
tjrehdrms123 <wwe5455@naver.com>
2023-08-07 18:55:24 +0900

    modfiy: class-validation DTO로 위치 변경

--- src/guardian/dto/request/create_guardian_dto.ts
+++ src/guardian/dto/request/create_guardian_dto.ts
@@ -1,7 +1,10 @@
 import { ApiProperty, PartialType, PickType } from '@nestjs/swagger'
+import { IsNotEmpty } from 'class-validator'
 import { GuardianEntity } from 'src/guardian/entities/guardian.entity'
 
 export class CreateGuardianDto extends PickType(GuardianEntity , ['name','phone_number'] as const) {
+    @IsNotEmpty({ message: '보호자 이름을 입력해주세요.' })
     name: string
+    @IsNotEmpty({ message: '보호자의 전화번호를 입력해주세요.' })
     phone_number: string
 }
