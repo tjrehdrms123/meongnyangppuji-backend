4291a1918602685038e0ee046bbee67a3310219e
tjrehdrms123 <wwe5455@naver.com>
2023-08-12 19:44:57 +0900

    feat: 추가한 모듈 성공 데이터 추가  ㄴ animal,qr

--- src/common/define/SuccessDefine.ts
+++ src/common/define/SuccessDefine.ts
@@ -1,7 +1,9 @@
-import { ResAnimalTypeDetailNameDto, ResAnimalTypeDto, ResAnimalTypeManyRowDto, ResAnimalTypeNameDto } from 'src/animal_type/dto/response/res_animal_type_dto';
+import { ResAnimalTypeDetailNameDto, ResAnimalTypeDto, ResAnimalTypeManyRowDto, ResAnimalTypeMetaData, ResAnimalTypeNameDto } from 'src/animal_type/dto/response/res_animal_type_dto';
 import { ResGuardianDto, ResGuardianMetaData, ResGuardianRowDto } from 'src/guardian/dto/response/res_guardian_dto';
 import { SuccessResponseOption } from '../decorators/SuccessResponse.decorator';
 import { ResUsersTypeDto } from 'src/users/dto/response/res_users_dto';
+import { ResQrDto, ResQrMetaData, ResQrRowDto } from 'src/qr/dto/response/res_qr_dto';
+import { ResAnimalDto, ResAnimalRowDto } from 'src/animal/dto/response/res_animal_dto';
 type Keys =
   | 'SUCCESS-1000'
   | 'SUCCESS-1001'
@@ -14,6 +16,13 @@ type Keys =
   | 'SUCCESS-3000'
   | 'SUCCESS-3001'
   | 'SUCCESS-3002'
+  | 'SUCCESS-4000'
+  | 'SUCCESS-4001'
+  | 'SUCCESS-4002'
+  | 'SUCCESS-5000'
+  | 'SUCCESS-5001'
+  | 'SUCCESS-5002'
+  | 'SUCCESS-5003'
   ;
 
 export const SuccessDefine: Record<
@@ -23,23 +32,23 @@ export const SuccessDefine: Record<
   // 1000 : AnimalType
   'SUCCESS-1000': {
     model: ResAnimalTypeDto,
-    exampleTitle: '애완동물 타입 생성 성공 예시',
-    exampleDescription: '애완동물 타입 생성 성공 예시',
+    exampleTitle: '반려동물 타입 생성 성공 예시',
+    exampleDescription: '반려동물 타입 생성 성공 예시',
   },
   'SUCCESS-1001': {
     model: ResAnimalTypeNameDto,
-    exampleTitle: '애완동물 타입 종류 조회 예시',
-    exampleDescription: '애완동물 타입 종류 조회 성공 예시',
+    exampleTitle: '반려동물 타입 종류 조회 예시',
+    exampleDescription: '반려동물 타입 종류 조회 성공 예시',
   },
   'SUCCESS-1002': {
     model: ResAnimalTypeDetailNameDto,
-    exampleTitle: '애완동물 상세 이름 조회 예시',
-    exampleDescription: '애완동물 상세 이름 조회 성공 예시',
+    exampleTitle: '반려동물 상세 이름 조회 예시',
+    exampleDescription: '반려동물 상세 이름 조회 성공 예시',
   },
   'SUCCESS-1003': {
     model: ResAnimalTypeDto,
-    exampleTitle: '애완동물 타입 내용 변경 예시',
-    exampleDescription: '애완동물 타입 내용 변경 성공 예시',
+    exampleTitle: '반려동물 타입 내용 변경 예시',
+    exampleDescription: '반려동물 타입 내용 변경 성공 예시',
   },
   'SUCCESS-1004': {
     model: ResAnimalTypeManyRowDto,
@@ -85,6 +94,47 @@ export const SuccessDefine: Record<
     exampleDescription: '유저 로그인 성공 예시',
     overwriteValue: {
       guardian_id: ResGuardianMetaData
+    }
+  },
+  // 4000 : QR
+  'SUCCESS-4000': {
+    model: ResQrDto,
+    exampleTitle: 'QR 생성 성공 예시',
+    exampleDescription: 'QR 생성 성공 예시'
+  },
+  'SUCCESS-4001': {
+    model: ResQrDto,
+    exampleTitle: 'QR 정보 수정 성공 예시',
+    exampleDescription: 'QR 정보 수정 성공 예시'
+  },
+  'SUCCESS-4002': {
+    model: ResQrRowDto,
+    exampleTitle: 'QR 삭제 성공 예시',
+    exampleDescription: 'QR 삭제 성공 예시'
+  },
+  // 5000 : Animal
+  'SUCCESS-5000': {
+    model: ResAnimalDto,
+    exampleTitle: '반려동물 생성 성공 예시',
+    exampleDescription: '반려동물 생성 성공 예시'
+  },
+  'SUCCESS-5001': {
+    model: ResAnimalDto,
+    exampleTitle: '반려동물 정보 수정 성공 예시',
+    exampleDescription: '반려동물 정보 수정 성공 예시'
+  },
+  'SUCCESS-5002': {
+    model: ResAnimalRowDto,
+    exampleTitle: '반려동물 삭제 성공 예시',
+    exampleDescription: '반려동물 삭제 성공 예시'
+  },
+  'SUCCESS-5003': {
+    model: ResAnimalRowDto,
+    exampleTitle: '반려동물 조회 성공 예시',
+    exampleDescription: '반려동물 조회 성공 예시',
+    overwriteValue: {
+      animal_type_id: ResAnimalTypeMetaData,
+      qr_id: ResQrMetaData
     }
   },
 };
