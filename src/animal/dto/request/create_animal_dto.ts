2e0609854d556ad308b420c24cb30cfd39b8e4e9
tjrehdrms123 <wwe5455@naver.com>
2023-08-13 20:45:02 +0900

    modfiy: typo

--- src/animal/dto/request/create_animal_dto.ts
+++ src/animal/dto/request/create_animal_dto.ts
@@ -1,4 +1,4 @@
-import { OmitType } from '@nestjs/swagger'
+import { PickType } from '@nestjs/swagger'
 import { AnimalEntity } from 'src/animal/entities/animal.entity';
 
-export class CreateAnimalDto extends OmitType(AnimalEntity , [] as const) {}
+export class CreateAnimalDto extends PickType(AnimalEntity , ['name','introduction','sex','profile_img','animal_type_id', 'qr_id'] as const) {}
