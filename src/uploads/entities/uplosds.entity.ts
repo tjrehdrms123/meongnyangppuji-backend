444cd7306bf1a3ee710f467c4012bb9d75c51a3d
tjrehdrms123 <wwe5455@naver.com>
2023-08-30 15:50:58 +0900

    modif: DB 구조 변경  ㄴ animal 이미지 컬럼 -> 업로드 엔티티를 별도로 생성해서 관리  ㄴ uploads api description 변경

--- src/uploads/entities/uplosds.entity.ts
+++ src/uploads/entities/uplosds.entity.ts
@@ -12,7 +12,7 @@ export class UploadsEntity extends CommonEntity {
     @IsNotEmpty({ message: '이미지를 넣어주세요.' })
     @ApiProperty({
       example: 'https://s3.ap-northeast-2.amazonaws.com/test.png',
-      description: 'QR 발행 여부 (0: 미발행, 1: 발행)',
+      description: '원본 이미지 S3 URL',
       required: true
     })
     img: string
@@ -20,7 +20,7 @@ export class UploadsEntity extends CommonEntity {
     @Column({ type: 'varchar', comment: '크롭 이미지', nullable: true })
     @ApiProperty({
       example: 'https://s3.ap-northeast-2.amazonaws.com/test.png',
-      description: 'QR 사용 여부 (0: 미사용, 1: 사용)'
+      description: '크롭된 이미지 S3 URL'
     })
     img_crop: string
 }
